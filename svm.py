33a6951caca3353ad29d0b6253502c792f138163
johnmcarter <jmcarter211@gmail.com>
2021-06-24 10:58:43 -0400

    updated svm code

--- svm.py
+++ svm.py
@@ -1,7 +1,7 @@
 '''
 John Carter
 Created: 2021/04/27 08:54:53
-Last modified: 2021/06/13 09:21:16
+Last modified: 2021/06/24 10:57:26
 Analyze system calls and network traffic data using a One-Class SVM
 '''
 
@@ -37,7 +37,7 @@ def my_svm(benign_data, malware_data, split, filename, data_type, malware, windo
     X_outliers = malware_data.values[:,:n_components]
     
     #roc.roc(data_type, malware)
-    metrics.f1(benign_data, malware_data)
+    #metrics.f1(benign_data, malware_data)
 
     # Train the one class SVM
     clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
@@ -54,50 +54,48 @@ def my_svm(benign_data, malware_data, split, filename, data_type, malware, windo
     n_error_outliers = y_pred_outliers[y_pred_outliers == 1].size
 
     print("Malware detection rate: ", ((X_outliers.size-n_error_outliers)/X_outliers.size)*100)
-    '''
-    # If we only have two pc's, then plot the classification
-    if n_components == 2:
-        plt.figure(figsize=(10,6))
+    
+    plt.figure(figsize=(10,6))
 
-        # plot the line, the points, and the nearest vectors to the plane
-        x_min = min(benign_data.values[:,:2].min(), (X_outliers[:, 0]).min()) - 5
-        x_max = max(benign_data.values[:,:2].max(), (X_outliers[:, 0]).max()) + 5
-        xx, yy = np.meshgrid(np.linspace(x_min, x_max, 500), np.linspace(x_min, x_max, 500))
-        
-        Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
-        Z = Z.reshape(xx.shape)
-        print(Z)
- 
-        plt.title("Pi-Router Novelty Detection - {} {} data {} grouping".format(data_type, malware, window))
-        plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), 0, 7), cmap='PuBu')
-        a = plt.contour(xx, yy, Z, levels=[0], linewidths=2, colors='darkred')
-        plt.contourf(xx, yy, Z, levels=[0, Z.max()], colors='palevioletred')
-        
-        s=40
-        b1 = plt.scatter(X_train[:, 0], X_train[:, 1], c='white', s=s, edgecolors='k')
-        b2 = plt.scatter(X_test[:, 0], X_test[:, 1], c='blueviolet', s=s, edgecolors='k')
-        b3 = plt.scatter(X_outliers[:, 0], X_outliers[:, 1], c='gold', s=s, edgecolors='k')
-        
-        plt.axis('tight')
+    # plot the line, the points, and the nearest vectors to the plane
+    x_min = min(benign_data.values[:,:2].min(), (X_outliers[:, 0]).min()) - 5
+    x_max = max(benign_data.values[:,:2].max(), (X_outliers[:, 0]).max()) + 5
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 500), np.linspace(x_min, x_max, 500))
+    
+    Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    print(Z)
 
-        plt.xlim((x_min, x_max))
-        plt.ylim((x_min, x_max))
-        plt.legend([a.collections[0], b1, b2, b3],
-                ["learned frontier", "training observations",
-                    "new regular observations", "new abnormal observations"],
-                loc="upper left",
-                prop=matplotlib.font_manager.FontProperties(size=11))
+    plt.title("Pi-Router Novelty Detection - {} {} data {} grouping".format(data_type, malware, window))
+    plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), 0, 7), cmap='PuBu')
+    a = plt.contour(xx, yy, Z, levels=[0], linewidths=2, colors='darkred')
+    plt.contourf(xx, yy, Z, levels=[0, Z.max()], colors='palevioletred')
+    
+    s=40
+    b1 = plt.scatter(X_train[:, 0], X_train[:, 1], c='white', s=s, edgecolors='k')
+    b2 = plt.scatter(X_test[:, 0], X_test[:, 1], c='blueviolet', s=s, edgecolors='k')
+    b3 = plt.scatter(X_outliers[:, 0], X_outliers[:, 1], c='gold', s=s, edgecolors='k')
+    
+    plt.axis('tight')
 
-        plt.xlabel("First Principal Component\nerror train: %d/%d ; errors novel regular: %d/%d ; " \
-            "errors novel abnormal: %d/%d ; Malware detection rate: %d/%d"
-        % (n_error_train, X_train.size, n_error_test, X_test.size, n_error_outliers, X_outliers.size, \
-            X_outliers.size - n_error_outliers, X_outliers.size))
-        plt.ylabel("Second Principal Component")
+    plt.xlim((x_min, x_max))
+    plt.ylim((x_min, x_max))
+    plt.legend([a.collections[0], b1, b2, b3],
+            ["learned frontier", "training observations",
+                "new regular observations", "new abnormal observations"],
+            loc="upper left",
+            prop=matplotlib.font_manager.FontProperties(size=11))
 
-        plt.savefig('figures/classification/%s' % filename)
-        print(INFO + "SVM figure saved to figures/classification/%s" % filename)
-        plt.close('all')
-    '''
+    plt.xlabel("First Principal Component\nerror train: %d/%d ; errors novel regular: %d/%d ; " \
+        "errors novel abnormal: %d/%d ; Malware detection rate: %d/%d"
+    % (n_error_train, X_train.size, n_error_test, X_test.size, n_error_outliers, X_outliers.size, \
+        X_outliers.size - n_error_outliers, X_outliers.size))
+    plt.ylabel("Second Principal Component")
+
+    plt.savefig('figures/classification/%s' % filename)
+    print(INFO + "SVM figure saved to figures/classification/%s" % filename)
+    plt.close('all')
+
     
 def main(benign_data_path, malware_data_path, split, 
         filename, data_type, malware, n_components=None):
